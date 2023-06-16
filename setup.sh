ecbd9bcaee0b836e45661d4fb69a9fe99377281a
Kyle Reinford <kyledeanreinford@gmail.com>
2023-06-16 10:26:24 -0500

    Updated README Added personal preferences dotfile options Creates SSH Key in script

--- setup.sh
+++ setup.sh
@@ -14,8 +14,23 @@ machinename=$1
 accountname=$2
 gituseremail=$3
 gitname=$4
+gitusername=${5:-false}
+ssh=${6:-false}
 
 echo "Setting up $accountname on $machinename for $gituseremail ($gitname)..."
+
+# optionally set up ssh key
+if [ $ssh == true ]
+then
+	echo "Creating an SSH key for you..."
+	ssh-keygen -t ed25519 -C "$gituseremail"
+	pbcopy < ~/.ssh/id_ed25519.pub
+	echo "\nThe public key has been added to your clipboard. \nAdd it to Github -> https://github.com/settings/ssh/new (cmd + double click) \n";
+	read -p "Press [Enter] after adding SSH key to Github."
+	echo "Authenticate to complete SSH setup."
+	ssh -T git@github.com
+	echo "";
+fi
 
 cat aliases >> ~/.aliases
 cat bash_profile >> ~/.bash_profile
@@ -39,10 +54,19 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 # brew
 brew analytics off
 
+# optionally grab dotfiles / personal preferences
+if [ $gitusername != false ]
+then
+	echo "Getting dotfiles from your GitHub account."
+	git clone https://github.com/$gitusername/dotfiles.git dotfiles
+	echo "";
+fi
+
 # git
 brew install git
 git config --global user.email $gituseremail
 git config --global user.name "$gitname"
+git config --global init.defaultBranch main
 
 # Java
 brew install openjdk
@@ -59,14 +83,14 @@ ruby-install --latest ruby
 
 # Go
 brew install -q go
-brew install -q --cask goland
+brew install -q --cask --no-quarantine goland
 
 # browsers
-brew install -q --cask firefox
-brew install -q --cask google-chrome
+brew install -q --cask --no-quarantine firefox
+brew install -q --cask --no-quarantine google-chrome
 
 # Google Cloud
-brew install -q --cask google-cloud-sdk
+brew install -q --cask --no-quarantine google-cloud-sdk
 echo 'source "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"' >> ~/.zprofile
 echo 'source "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"' >> ~/.zprofile
 
@@ -74,21 +98,24 @@ echo 'source "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/pa
 brew tap heroku/brew && brew install heroku
 
 # databases
+brew tap homebrew/core
 brew install -q postgresql@14
 brew services start postgresql
 brew install mysql
 
 # other tools
 brew install wget watch tree
-brew install --cask keepingyouawake jetbrains-toolbox slack zoom pop docker rectangle 1password iterm2
-brew install --cask pastebot krisp
-brew install -q --cask sublime-text
-brew install -q --cask intellij-idea
-brew install -q --cask rubymine
+brew install --cask --no-quarantine keepingyouawake jetbrains-toolbox slack zoom pop docker rectangle 1password iterm2
+brew install --cask --no-quarantine pastebot krisp
+brew install -q --cask --no-quarantine sublime-text
+brew install -q --cask --no-quarantine intellij-idea
+brew install -q --cask --no-quarantine rubymine
 brew install -q flyway
 brew install -q kubernetes-cli
 brew install -q buildpacks/tap/pack
 brew install -q gettext
+
+source dotfiles/app_preferences.sh
 
 # set menu clock
 defaults write com.apple.menuextra.clock "DateFormat" 'EEE MMM d  h:mm:ss a'
@@ -133,6 +160,7 @@ defaults write com.apple.finder QuitMenuItem -bool true
 # Add home and workspace to finder sidebar, remove others
 brew install mysides 
 mysides add $accountname file:///Users/$accountname
+mkdir ~/workspace
 mysides add workspace file:///Users/$accountname/workspace
 mysides remove Recents
 mysides remove Applications
@@ -152,11 +180,13 @@ defaults write com.googlecode.iterm2 PromptOnQuit -bool true
 # "import" settings
 cp com.googlecode.iterm2.plist ~/Library/Preferences
 
+source dotfiles/machine_preferences.sh
+
 mkdir ~/bin
 echo "Adding local /bin to /etc/paths, may be prompted for root password"
 sudo sh -c "echo /Users/$accountname/bin >> /etc/paths"
 
 echo "****************************"
 echo "$machinename setup complete!"
-echo "Please restart now"
+echo "Please restart your machine now"
 echo "****************************"
